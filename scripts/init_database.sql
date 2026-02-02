274da3676e981ad1830dfb98e16f42f2cfd4473f
AyushRana47 <ayushrana.4747@gmail.com>
2026-02-02 23:54:40 +0530

    Update and rename placeholder to init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,14 @@
+-- Create the 'DataWarehouse' database
+create database DataWarehouse;
+go
+
+use DataWarehouse;
+go
+
+-- Create Schemas
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
